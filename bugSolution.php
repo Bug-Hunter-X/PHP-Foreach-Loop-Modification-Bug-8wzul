3ef2c597975949001b9f2e5a4914e183adb14ff4
BugHunter-X <askzenai@gmail.com>
2024-12-08 01:14:04 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,11 @@
+function foo(array $arr): void {
+  $keysToRemove = [];
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      $keysToRemove[] = $key;
+    }
+  }
+  foreach ($keysToRemove as $key) {
+    unset($arr[$key]);
+  }
+}
